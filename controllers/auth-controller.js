42e6b41b28191da2d85dedf021f04df228025a0f
Erin Templeton <erindtempleton@gmail.com>
2024-04-30 11:58:24 -0700

    Add login endpoint

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -8,7 +8,7 @@ const register = async (req, res) => {
     
     if (!first_name || !last_name || !email || !password ) {
         return res.status(400).json({
-            message: "Missing registration form data"
+            message: "Missing registration form data",
         });
     }
 
@@ -30,12 +30,54 @@ const register = async (req, res) => {
         res.status(201).json(newUser);
     } catch (error) {
         res.status(500).json({
-            message: `Unable to register user: ${error}`
+            message: `Unable to register user: ${error}`,
+        })
+    }   
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password ) {
+        return res.status(400).json({
+            message: "Missing login form data",
+        });
+    }
+
+    try {
+        const user = await knex("users")
+            .where({ email: email })
+            .first();
+    
+        if (!user) {
+            return res.status(401).json({
+                message: "User not found",
+            })
+        }
+
+        const isPasswordMatch = user.password === password;
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Email or password is incorrect",
+            })
+        }
+
+        const token = jwt.sign({
+            id: user.id,
+            email: user.email,
+        }, SECRET_KEY)
+
+        res.status(200).json({
+            access_token: token,
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to login: ${error}`
         })
     }
-    
 }
 
 module.exports = {
     register,
+    login
 }
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,4 +4,7 @@ const authController = require('../controllers/auth-controller');
 router.route("/register")
     .post(authController.register);
 
+router.route("/login")
+    .post(authController.login);
+
 module.exports = router;
